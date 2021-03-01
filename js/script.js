bba9f4f2ad2869613065d7ab2fc323d6aafa83c6
kellyph234 <kellyph234@gmail.com>
2021-03-01 15:55:34 -0800

    script.js
    adding a script page

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
   alert("Hello, welcome to my creative corner!");
+
+var text1 = "Hello";
+var text2 = "Hello, spaces are also OK!";
+var text3 = 'Single quotes are valid too.';
+
+var numberInteger = 456;
+var numberInteger2 = -345; // This is negative integer
+var numberFloat = 50.25;
+
+console.log (numberInteger)
