5682c620937c19cffd09e92e9ee40913e9ad8b50
kellyph234 <kellyph234@gmail.com>
2021-03-02 18:11:49 -0800

    JS 2.3
    adds JS code to page to work with hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
   alert("Hello, welcome to my creative corner!");
 
-var text1 = "Hello";
+var text1 = "Hello, you found me :)";
 var text2 = "Hello, spaces are also OK!";
 var text3 = 'Single quotes are valid too.';
 
@@ -8,4 +8,14 @@ var numberInteger = 456;
 var numberInteger2 = -345; // This is negative integer
 var numberFloat = 50.25;
 
-console.log (numberInteger)
+console.log (text1)
+
+//2.3//
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+}   else {
+    x.className = 'navtoggle';
+}
