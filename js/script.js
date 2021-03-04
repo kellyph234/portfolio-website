df02848c4a1233bb27e482c19496f5ad065c11f5
kellyph234 <kellyph234@gmail.com>
2021-03-03 17:23:05 -0800

    2.3 changes
    2.3 feedback changes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-  alert("Hello, welcome to my creative corner!");
-
-var text1 = "Hello, you found me :)";
-var text2 = "Hello, spaces are also OK!";
-var text3 = 'Single quotes are valid too.';
-
-var numberInteger = 456;
-var numberInteger2 = -345; // This is negative integer
-var numberFloat = 50.25;
-
-console.log (text1)
 
 //2.3//
 
@@ -18,4 +7,5 @@ function menuToggle() {
     x.className += ' responsive';
 }   else {
     x.className = 'navtoggle';
+}
 }
