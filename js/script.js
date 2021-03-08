0e8591ec192051099a1b2704bfb6dd340fff0123
kellyph234 <kellyph234@gmail.com>
2021-03-07 18:04:35 -0800

    activate menu toggle
    Menu toggle on line 30 is activated.

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
-}   else {
+    x.className += 'responsive';
+  } else {
     x.className = 'navtoggle';
-}
+  }
 }
