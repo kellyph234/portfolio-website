2b434d8b4105ccd8c9cee15d379b935582b926b0
kellyph234 <kellyph234@gmail.com>
2021-03-07 19:22:32 -0800

    add and center
    adds space to code in js. center footer on page

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
   if (x.className === 'navtoggle') {
-    x.className += 'responsive';
+    x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
   }
